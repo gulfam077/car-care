4bc7ca5af2bd012640c978d0a799d22110a6dd1c
Rai Gulfam <raigulfam077@gmail.com>
2025-10-13 09:37:39 +0500

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 
 
 // Step 3: Choose a port (a number for your server to listen on)
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
 
 app.use(express.static('public'));
 
